2b4e7ece7cad95a118f3573b324103a6c6813c77
oHoangQuynhAnh <hquynhanh19@gmail.com>
2019-03-15 11:09:35 +0700

    Implementing basic login logout feature (#5)

--- blog_project/blog_app/urls.py
+++ blog_project/blog_app/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
+from django.conf.urls import include, url
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
   path("", views.index, name="index"),
   path("signup", views.signup, name="signup"),
+  url(r"^login/$", auth_views.LoginView.as_view(template_name="blog_app/login.html"), name="login"),
+  url(r"^logout/$", auth_views.LogoutView.as_view(), {"next_page": "/"}, name="logout"),
 ]
--- blog_project/blog_app/views.py
+++ blog_project/blog_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import Http404
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
