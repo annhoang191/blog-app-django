735be6807735515c91ea1edcd37871dabe283661
oHoangQuynhAnh <hquynhanh19@gmail.com>
2019-03-15 10:00:37 +0700

    Implementing basic signup with django auth (#4)

--- blog_project/blog_app/models/__init__.py
+++ blog_project/blog_app/models/__init__.py
@@ -1,3 +1,2 @@
-from .user import User
 from .blog import Blog
 from .comment import Comment
--- blog_project/blog_app/models/blog.py
+++ blog_project/blog_app/models/blog.py
@@ -1,5 +1,5 @@
 from django.db import models
-from .user import User
+from django.contrib.auth.models import User
 
 class Blog(models.Model):
   title = models.CharField(max_length=64)
